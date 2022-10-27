09fec293590fe531ba99e1dc9401a6bbaade46eb
Nurtimax <maksatbeknurtilek@gmail.com>
2022-10-26 22:50:01 -0700

    open card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import Card from "./components/card/Card";
 import Todo from "./components/todo/Todo";
 
 function App() {
+  const [open, setOpen] = useState(true);
   return (
     <div className="App">
-      {/* <Todo/> */}
-      <Card />
+      {open ? <Todo /> : <Card />}
+
+      <button onClick={() => setOpen((prevState) => !prevState)}>{open? 'card' : 'todo'}</button>
     </div>
   );
 }
